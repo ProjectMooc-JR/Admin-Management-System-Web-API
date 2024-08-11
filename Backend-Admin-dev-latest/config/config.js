b035473b77a4e2a8549a52841417d6042048220c
Zhongqi Wei <Zhongqi.Wei@outlook.com>
2024-08-11 12:01:12 +0800

    some changes modified

--- Backend-Admin-dev-latest/config/config.js
+++ Backend-Admin-dev-latest/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     username: 'root',
     password: appConfig.mysqlConfig.password,
-    database: 'workshop',
+    database: 'adminmanagementsystemdb',
     host: '127.0.0.1',
     dialect: 'mysql'
   }
