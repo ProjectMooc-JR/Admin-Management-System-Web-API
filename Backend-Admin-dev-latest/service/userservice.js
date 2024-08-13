3136d27a52edb204af97ad7f64b118c86e7e6759
FrankQIAN <100653166+FrankQian1993@users.noreply.github.com>
2024-08-13 17:01:36 +0930

    Update userservice.js -- add user and get user by name

--- Backend-Admin-dev-latest/service/userservice.js
+++ Backend-Admin-dev-latest/service/userservice.js
@@ -1,6 +1,7 @@
 
 const { db } = require("../db/mysqldb.js");
 
+//get user by name
 var getUserbyNameAsync = async (name) => {
   let sql = "SELECT * FROM user where username=? ";
   let result = await db.query(sql, [name]);
@@ -12,48 +13,71 @@ var getUserbyNameAsync = async (name) => {
     user.email = result[0][0].email;
     user.age = result[0][0].age;
     user.gender = result[0][0].gender;
+    user.avatar = result[0][0].avatar;
+    user.nickname = result[0][0].nickname;
+    user.access = result[0][0].access;
+    user.active = result[0][0].active;
   }
   return { isSuccess: true, message: "", data: user };
 };
 
-
-var getUserListAsync = async (page, pageSize) => {
-  let countSql = "SELECT count(*) total FROM user; ";
-  let resultCount = await db.query(countSql);
-  let total = resultCount[0][0].total;
-  if (total == 0) {
-    return { isSuccess: true, message: "", data: { items: [], total: 0 } };
-  }
-  let sql = "SELECT * FROM user limit ? offset ? ;";
-  let resultData = await db.query(sql, [pageSize, (page - 1) * pageSize]);
-
-  let userlist = [];
-  if (resultData[0].length > 0) {
-    resultData[0].forEach((element) => {
-      let user = { id: 0 };
-      user.id = element.id;
-      user.username = element.username;
-      //user.password = element.password;
-      user.email = element.email;
-      user.address = element.address;
-      user.age = element.age;
-      user.gender = element.gender;
-      user.avatar = element.avatar;
-      user.access = element.access;
-      userlist.push(user);
-    });
-  }
-  return {
-    isSuccess: true,
-    message: "",
-    data: { items: userlist, total: total },
-  };
+// add user
+const addUserAsync = async (user) => {
+  let sql =
+    "insert into users (username, password, email, address, age, gender, avatar, nickname, access, active) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  let [result] = await db.query(sql, [
+    user.username,
+    user.password,
+    user.email,
+    user.address,
+    user.age,
+    user.gender,
+    user.avatar,
+    user.nickname,
+    user.access,
+    user.active,
+  ]);
+  return { isSuccess: true, message: "add user successfully" };
 };
+
+
+// var getUserListAsync = async (page, pageSize) => {
+//   let countSql = "SELECT count(*) total FROM user; ";
+//   let resultCount = await db.query(countSql);
+//   let total = resultCount[0][0].total;
+//   if (total == 0) {
+//     return { isSuccess: true, message: "", data: { items: [], total: 0 } };
+//   }
+//   let sql = "SELECT * FROM user limit ? offset ? ;";
+//   let resultData = await db.query(sql, [pageSize, (page - 1) * pageSize]);
+
+//   let userlist = [];
+//   if (resultData[0].length > 0) {
+//     resultData[0].forEach((element) => {
+//       let user = { id: 0 };
+//       user.id = element.id;
+//       user.username = element.username;
+//       //user.password = element.password;
+//       user.email = element.email;
+//       user.address = element.address;
+//       user.age = element.age;
+//       user.gender = element.gender;
+//       user.avatar = element.avatar;
+//       user.access = element.access;
+//       userlist.push(user);
+//     });
+//   }
+//   return {
+//     isSuccess: true,
+//     message: "",
+//     data: { items: userlist, total: total },
+//   };
+// };
 
 
 
 module.exports = {
-  getUserListAsync,
-  getUserbyNameAsync
- 
+  //getUserListAsync,
+  getUserbyNameAsync,
+  addUserAsync
 };
