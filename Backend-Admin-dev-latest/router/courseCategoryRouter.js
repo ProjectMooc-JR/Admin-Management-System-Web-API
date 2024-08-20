44d6bc9b8021fb50faff1edaf6bbb2c3c2f6fd8d
lily <lily.jiang@institutedata.com>
2024-08-20 23:54:01 +1000

    v1.0.0.7- Lily- Add swagger comments for course category

--- Backend-Admin-dev-latest/router/courseCategoryRouter.js
+++ Backend-Admin-dev-latest/router/courseCategoryRouter.js
@@ -9,7 +9,41 @@ const courseCategoryRule=[
     check('courseCategoryId').optional().isInt().withMessage('Category ID must be an integer'),
 ]
 
+/**
+ * @openapi
+ * '/api/courseCategory':
+ *  post:
+ *     tags:
+ *     - course Category
+ *     summary: Add a new courseCategory
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               CategoryName:
+ *                 type: string
+ *               Level:
+ *                 type: number
+ *               ParentID:
+ *                 type: number
+ *               Notes:
+ *                 type: string
+ *     responses:
+ *      201:
+ *        description: courseCategory added successfully
+ *      400:
+ *        description: Bad Request
+ *      500:
+ *        description: Server Error
+ */
 router.post('/',courseCategoryController.addCourseCategoryAsync);
+
+
 router.put('/:courseCategoryId',courseCategoryController.updateCourseCategoryAsync);
 router.delete("/:courseCategoryId",courseCategoryController.deleteCourseCategoryByIdAsync);
 router.get("/:courseCategoryId",courseCategoryController.getCourseCategoryByIdAsync)
