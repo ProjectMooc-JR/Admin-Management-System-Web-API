61c104f0b51e7b606dc20f3186bf20639194f6cb
Rusell Pan <russell1997100188@gmail.com>
2024-08-25 15:27:59 +1000

    fix some bug

--- Backend-Admin-dev-latest/router/courseCategoryRouter.js
+++ Backend-Admin-dev-latest/router/courseCategoryRouter.js
@@ -1,140 +1,190 @@
-const { db } = require("../db/mysqldb.js");
-//add course category
-const addCourseCategoryAsync = async (courseCategory) => {
-  const { CategoryName, Level, ParentID, Notes } = courseCategory;
-  const insertSql = "INSERT INTO coursecategories (CategoryName, Level, ParentID, Notes) VALUES (?, ?, ?, ?);";
-  const querySql = "select * from coursecategories where CategoryName = ? ;";
-  const values = [CategoryName, Level, ParentID, Notes];
-  const connection = await db.getConnection();
-  try {
-    await connection.beginTransaction();
-    const [result] = await db.query(querySql, [CategoryName]);
-    if (result.length > 0) {
-      return { isSuccess: false, msg: "course category already exits", data: null }
-    }
-    const [rows] = await db.query(insertSql, values);
-    return { isSuccess: true, msg: "insert successfully", data: rows.insertId };
-  } catch (error) {
-    throw new Error(`Error adding course: ${error.message}`);
-  }
-};
-// update course category
-const updateCourseCategoryAsync = async (courseCategory) => {
-  const { id, CategoryName, Level, ParentID, Notes } = courseCategory;
+const express = require('express');
+const router = express.Router();
+const courseCategoryController = require('../controller/courseCategoryController'); // Import the course controller
+const { commonValidate } = require('../middleware/expressValidator'); // Import the validation middleware
+const { check, body, param } = require('express-validator');
 
-  const selectSql = "SELECT * FROM coursecategories WHERE ID = ?;";
-  const updateSql = `UPDATE coursecategories 
-                     SET CategoryName = ?, Level = ?, ParentID = ?, Notes = ?
-                     WHERE ID = ?`;
-  const connection = await db.getConnection();
 
-  try {
-    await connection.beginTransaction();
-    const [rows] = await db.query(selectSql, [id]);
-    if (rows.length === 0) {
-      await connection.rollback();
-      return { isSuccess: false, msg: "course category not exist", data: null };
-    }
-    const values = [CategoryName, Level, ParentID, Notes, id];
+/**
+* @openapi
+* '/api/course-category':
+*  post:
+*     tags:
+*     - Course Category
+*     summary: Add a new course category
+*     description: Creates a new course category with the provided details.
+*     requestBody:
+*      required: true
+*      content:
+*        application/json:
+*           schema:
+*            type: object
+*            required:
+*              - CategoryName
+*              - Level
+*              - ParentID
+*            properties:
+*              CategoryName:
+*                type: string
+*                example: Science
+*              Level:
+*                type: string
+*                example: Beginner
+*              ParentID:
+*                type: string
+*                example: 123
+*              Notes:
+*                type: string
+*                example: This is a science course category
+*     responses:
+*      201:
+*        description: Course category created successfully
+*      400:
+*        description: Validation error
+*/
 
-    const [updateResult] = await db.query(updateSql, values);
-    if (updateResult.affectedRows !== 1) {
-      await connection.rollback();
-      return { isSuccess: false, msg: "update failed", data: null };
-    }
-    const [updatedRows] = await db.query(selectSql, [id]);
-    await connection.commit();
-    return { isSuccess: true, msg: "update successfully", data: updatedRows };
-  } catch (error) {
-    await connection.rollback();
-    return { isSuccess: false, msg: error.message, data: null };
-  } finally {
-    connection.release();
-  }
-};
-//delete course category by id
-const deleteCourseCategoryByIdAsync = async (courseCategoryId) => {
-  const connection = await db.getConnection()
-  const checkParentIdSql = "Select count(*) as count from coursecategories where ParentID=?";
-  const checkCourse = "Select count(*) as count from courses where CategoryID=? ";
-  const deleteSql = "Delete FROM coursecategories WHERE ID = ? ;";
-  try {
-    await connection.beginTransaction();
-    const [result] = await connection.query(checkParentIdSql, [courseCategoryId])
-    if (result[0].count !== 0) {
-      throw new Error("This category is the parent class of other course categories, which cannot be deleted");
-    }
-    const [result1] = await connection.query(checkCourse, [courseCategoryId]);
-    if (result1[0].count !== 0) {
-      throw new Error("This category is the parent class of other course, which cannot be deleted");
-    }
-    const [rows] = await connection.query(deleteSql, [courseCategoryId]);
-    if (rows.affectedRows === 0) {
-      return { isSuccess: false, msg: "course category not exist", data: null }
-    }
-    await connection.commit();
-    return { isSuccess: true, msg: "course category delete successfully", data: null };
-  } catch (error) {
-    await connection.rollback();
-    throw new Error(`Error deleting course category: ${error.message}`);
-  } finally {
-    connection.release();
-  }
-};
-// get all course category
-const getAllCourseCategoryAsync = async (page, pageSize) => {
-  const count = "SELECT count(*) total FROM coursecategories";
-  let sql = "SELECT * FROM coursecategories limit ? offset ? ;";
-  try {
-    let resultCount = await db.query(count);
-    let total = resultCount[0][0].total;
-    if (total == 0) {
-      return { isSuccess: true, message: "", data: { items: [], total: 0 } };
-    }
-    let resultData = await db.query(sql, [pageSize, (page - 1) * pageSize]);
-    console.log(resultData)
-    let courseCategoriesList = [];
-    if (resultData[0].length > 0) {
-      resultData[0].forEach((element) => {
-        let courseCategory = { id: 0 };
-        courseCategory.id = element.ID;
-        courseCategory.categoryName = element.CategoryName;
-        courseCategory.Level = element.Level;
-        courseCategory.ParentID = element.ParentID;
-        courseCategory.Notes = element.Notes;
-        courseCategoriesList.push(courseCategory);
-      });
-    }
-    return {
-      isSuccess: true,
-      message: "",
-      data: { items: courseCategoriesList, total: total },
-    };
-  } catch (error) {
-    throw new Error(`Error fetching course category: ${error.message}`);
+/**
+* @openapi
+* '/api/course-category/{courseCategoryId}':
+*  put:
+*     tags:
+*     - Course Category
+*     summary: Update a course category
+*     description: Updates the course category with the given ID.
+*     parameters:
+*      - name: courseCategoryId
+*        in: path
+*        required: true
+*        schema:
+*          type: string
+*          example: 123
+*     requestBody:
+*      required: true
+*      content:
+*        application/json:
+*           schema:
+*            type: object
+*            properties:
+*              CategoryName:
+*                type: string
+*                example: Science
+*              Level:
+*                type: string
+*                example: Beginner
+*              ParentID:
+*                type: string
+*                example: 456
+*              Notes:
+*                type: string
+*                example: Updated notes
+*     responses:
+*      200:
+*        description: Course category updated successfully
+*      400:
+*        description: Validation error
+*
+*  delete:
+*     tags:
+*     - Course Category
+*     summary: Delete a course category
+*     description: Deletes the course category with the given ID.
+*     parameters:
+*      - name: courseCategoryId
+*        in: path
+*        required: true
+*        schema:
+*          type: string
+*          example: 123
+*     responses:
+*      200:
+*        description: Course category deleted successfully
+*      400:
+*        description: Validation error
+*
+*  get:
+*     tags:
+*     - Course Category
+*     summary: Get a course category by ID
+*     description: Retrieves the details of a course category by its ID.
+*     parameters:
+*      - name: courseCategoryId
+*        in: path
+*        required: true
+*        schema:
+*          type: string
+*          example: 123
+*     responses:
+*      200:
+*        description: Course category retrieved successfully
+*      400:
+*        description: Validation error
+*/
 
-  }
+/**
+* @openapi
+* '/api/course-category/{page}/{pageSize}':
+*  get:
+ *     tags:
+ *     - Course Category
+ *     summary: Get all course categories
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *      - name: page
+ *        in: path
+ *        description: page
+ *        required: true
+ *      - name: pageSize
+ *        in: path
+ *        description: pageSize
+ *        required: true
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully
+ *      400:
+ *        description: Bad Request
+ *      401:
+ *        description: Unauthorized
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+*/
 
-};
-// get course category by id
-const getCourseCategoryById = async (courseId) => {
-  const sql = "SELECT * FROM coursecategories WHERE ID = ?";
-  try {
-    const [rows] = await db.query(sql, [courseId]);
-    if (rows.length === 0) {
-      return { isSuccess: false, msg: "course category not exist", data: null };
-    }
-    return { isSuccess: true, msg: "course category delete successfully", data: rows };
-    ;
-  } catch (error) {
-    throw new Error(`Error fetching course category: ${error.message}`);
-  }
-};
+router.post('/', commonValidate([
+    body("CategoryName").notEmpty().withMessage("CategoryName is required"),
+    body("Level").notEmpty().withMessage("Level is required"),
+    body("ParentID").notEmpty().withMessage("ParentID is required"),
+    body("Notes").optional()
+]), courseCategoryController.addCourseCategoryAsync);
 
-module.exports = {
-  getCourseCategoryById,
-  getAllCourseCategoryAsync,
-  deleteCourseCategoryByIdAsync,
-  addCourseCategoryAsync,
-  updateCourseCategoryAsync
-}
+router.put('/:courseCategoryId', commonValidate([
+    param("courseCategoryId").notEmpty().withMessage("courseCategoryId is required"),
+    body("CategoryName").optional(),
+    body("Level").optional(),
+    body("ParentID").optional(),
+    body("Notes").optional()
+]), courseCategoryController.updateCourseCategoryAsync);
+
+router.delete("/:courseCategoryId", commonValidate([
+    param("courseCategoryId").notEmpty().withMessage("courseCategoryId is required"),
+]), courseCategoryController.deleteCourseCategoryByIdAsync);
+
+router.get("/:courseCategoryId", commonValidate([
+    param("courseCategoryId").notEmpty().withMessage("courseCategoryId is required"),
+]), courseCategoryController.getCourseCategoryByIdAsync)
+
+router.get("/:page/:pageSize",commonValidate([
+    param("page")
+      .notEmpty()
+      .isInt({ allow_leading_zeroes: false, min: 1 })
+      .withMessage("Not a valid page"),
+    param("pageSize")
+      .notEmpty()
+      .isInt({ allow_leading_zeroes: false, min: 1 })
+      .withMessage("Not a valid page"),
+]), courseCategoryController.getAllCourseCategoryAsync)
+
+
+module.exports = router
+
