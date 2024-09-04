fbd372780b7ccf0be54cc7ac567de5c7a388288d
lily <lily.jiang@institutedata.com>
2024-09-04 18:54:54 +1000

    v1.0.0.10- Fixed issue with courseCategory API not connecting to the UI

--- Backend-Admin-dev-latest/controller/courseCategoryController.js
+++ Backend-Admin-dev-latest/controller/courseCategoryController.js
@@ -3,22 +3,24 @@ const courseCategoryService = require("../service/courseCategoryService");
 const addCourseCategoryAsync = async (req, res) => {
   let courseCategory = {
     CategoryName: req.body.CategoryName,
-    Level: req.body.Level?req.body.Level:0,
-    ParentID: req.body.ParentID?req.body.ParentID:0,
+    Level: req.body.Level ? req.body.Level : 0,
+    ParentID: req.body.ParentID ? req.body.ParentID : 0,
     Notes: req.body.Notes ? req.body.Notes : "",
   };
-  let result = await courseCategoryService.addCourseCategoryAsync(courseCategory);
+  let result = await courseCategoryService.addCourseCategoryAsync(
+    courseCategory
+  );
   return res.sendCommonValue(
     result.data,
     result.msg,
     result.isSuccess ? 201 : 400
-    );
+  );
 };
 
 const getAllCourseCategoryAsync = async (req, res) => {
   const result = await courseCategoryService.getAllCourseCategoryAsync();
   res.sendCommonValue(
-    result.data,
+    { items: result.data, total: result.data.length },
     result.msg,
     result.isSuccess ? 200 : 400
   );
@@ -27,11 +29,7 @@ const getAllCourseCategoryAsync = async (req, res) => {
 const getCourseCategoryByIdAsync = async (req, res) => {
   const id = req.params.courseCategoryId;
   const result = await courseCategoryService.getCourseCategoryById(id);
-  res.sendCommonValue(
-    result.data,
-    result.msg,
-    result.isSuccess ? 200 : 400
-  );
+  res.sendCommonValue(result.data, result.msg, result.isSuccess ? 200 : 400);
 };
 
 const updateCourseCategoryAsync = async (req, res) => {
@@ -41,22 +39,16 @@ const updateCourseCategoryAsync = async (req, res) => {
   courseCategory.Level = req.body.Level;
   courseCategory.ParentID = req.body.ParentID;
   courseCategory.Notes = req.body.Notes;
-  let result = await courseCategoryService.updateCourseCategoryAsync(courseCategory);
-  res.sendCommonValue(
-    result.data,
-    result.msg,
-    result.isSuccess ? 200 : 400
+  let result = await courseCategoryService.updateCourseCategoryAsync(
+    courseCategory
   );
+  res.sendCommonValue(result.data, result.msg, result.isSuccess ? 200 : 400);
 };
 
 const deleteCourseCategoryByIdAsync = async (req, res) => {
   const id = req.params.courseCategoryId;
   const result = await courseCategoryService.deleteCourseCategoryByIdAsync(id);
-  res.sendCommonValue(
-    result.data,
-    result.msg,
-    result.isSuccess ? 200 : 400
-  );
+  res.sendCommonValue(result.data, result.msg, result.isSuccess ? 200 : 400);
 };
 
 module.exports = {
@@ -64,5 +56,5 @@ module.exports = {
   getAllCourseCategoryAsync,
   getCourseCategoryByIdAsync,
   updateCourseCategoryAsync,
-  deleteCourseCategoryByIdAsync
-}
+  deleteCourseCategoryByIdAsync,
+};
